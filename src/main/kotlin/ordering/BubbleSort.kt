dd8d4c28ed51469f0eff9e022380e004bff3a64f
Ing Brayan Martinez <brayanmartinez827@gmail.com>
2024-06-30 19:18:12 -0400

    mejorar version

--- src/main/kotlin/ordering/BubbleSort.kt
+++ src/main/kotlin/ordering/BubbleSort.kt
@@ -1,21 +1,18 @@
 package ordering
 
-fun bubbleSort(data: MutableList<Int>): Void? {
-    var swap_check = true
-    val n = data.size
-
-    var i = 0
-    while ((i < n) && (swap_check)) {
-        swap_check = false
-        for (j in 0 until n - 1 - i) {
+fun bubbleSort(data: MutableList<Int>) {
+    for (i in 0 until data.size - 1) {
+        var breakCount = 0
+        for (j in 0 until data.size - 1 - i) {
             if (data[j] > data[j + 1]) {
-                swap_check = true
                 swap(j, j + 1, data)
+                breakCount++
             }
         }
-        i++
+        if (breakCount == 0) {
+            break
+        }
     }
-    return null
 }
 
 private fun swap(first: Int, second: Int, data: MutableList<Int>) {
